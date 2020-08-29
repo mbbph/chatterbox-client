9bc2c78bbf1dd3dae8b7dfd1b80421e45a4c386c
Lisa Hou <lqnhou@gmail.com>
2020-08-29 10:50:23 -0700

    added basic submitting functionality

--- client/scripts/messages.js
+++ client/scripts/messages.js
@@ -1,4 +1,19 @@
 var Messages = {
+  submit: function() {
+    var message = {
+      username: App.username,
+      text: $('#message').val(),
+      roomname: 'lobby'
+    };
 
+    Parse.create(message);
+    $('#message').val('');
+  }
 
 };
+
+// var message = {
+//   username: 'shawndrost',
+//   text: 'trololo',
+//   roomname: '4chan'
+// };
